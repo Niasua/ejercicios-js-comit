263a249cc057b91674ab520bdb463bb38e3c5b03
niasua <niasua@Nico-PC.>
2025-03-07 23:28:26 +0000

    ej5 completed

---
+++ ej5/ej5.js
@@ -0,0 +1,13 @@
+let nombre = 'Nicolás';
+let apellido = 'Suarez';
+let edad = 20;
+let fechaDeNacimiento = '24-10-2004';
+let direccion = "calle 842";
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(fechaDeNacimiento);
+console.log(direccion);
+
+
